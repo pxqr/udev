27cd7ccd5d74e272c3715503cd772e47b79c6dfa
Sam Truzjan <pxqr.sta@gmail.com>
2013-10-09 02:15:17 +0400

    Export all modules in the main module

--- src/System/UDev.hs
+++ src/System/UDev.hs
@@ -1,6 +1,31 @@
-module System.UDev ( ) where
+-- |
+--   Copyright   :  (c) Sam Truzjan 2013
+--   License     :  BSD3
+--   Maintainer  :  pxqr.sta@gmail.com
+--   Stability   :  experimental
+--   Portability :  portable
+--
+module System.UDev
+       ( module System.UDev.Context
+       , module System.UDev.Device
+       , module System.UDev.Enumerate
+       , module System.UDev.HWDB
+       , module System.UDev.List
+       , module System.UDev.Monitor
+       , module System.UDev.Queue
+       , module System.UDev.Util
+       ) where
 
 import Data.Monoid
+
+import System.UDev.Context
+import System.UDev.Device
+import System.UDev.Enumerate
+import System.UDev.HWDB
+import System.UDev.List
+import System.UDev.Monitor
+import System.UDev.Queue
+import System.UDev.Util
 
 
 type Subsystem = ()
