3e562d0982512692c849c6aac45fb0243d9fd374
Sam Truzjan <pxqr.sta@gmail.com>
2013-10-09 02:12:57 +0400

    Hide internal module

--- src/System/UDev/Types.hs
+++ src/System/UDev/Types.hs
@@ -1,3 +1,12 @@
+-- |
+--   Copyright   :  (c) Sam Truzjan 2013
+--   License     :  BSD3
+--   Maintainer  :  pxqr.sta@gmail.com
+--   Stability   :  experimental
+--   Portability :  portable
+--
+--   Internal definitions; do not export this module.
+--
 module System.UDev.Types
        ( Ref (..)
        ) where
