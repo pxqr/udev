ff772d0171104fe8520dca0d3cb139d91d8571b5
Leif Warner <abimelech@gmail.com>
2018-05-17 10:43:04 -0700

    Cleanup monitor example, drop dep on select package.
    You don't need select to block on a file descriptor, just use
    threadWaitRead (which the GHC runtime will delegate to epoll on Linux).

--- examples/monitor.hs
+++ examples/monitor.hs
@@ -1,17 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import Prelude as P
-
-import Control.Monad
-import Data.ByteString.Char8 as BC
+import Control.Monad (forever)
+import Control.Concurrent (threadWaitRead)
 
 import System.UDev.Context
 import System.UDev.Device
 import System.UDev.Monitor
-import System.Posix.IO.Select
-import System.Posix.IO.Select.Types
-
 
 dumpDeviceInfo :: Device -> IO ()
 dumpDeviceInfo dev = do
@@ -22,21 +17,16 @@ dumpDeviceInfo dev = do
   print $ getSysnum    dev
   print $ getDevnode    dev
   print $ getAction    dev
-  P.putStrLn $ "device number: " ++ show (getDevnum dev)
+  putStrLn $ "device number: " ++ show (getDevnum dev)
 
 main :: IO ()
-main = do
-  withUDev $ \ udev -> do
-    setLogPriority udev LogDebug
-    setLogger udev defaultLogger
-    monitor <- newFromNetlink udev UDevId
-    enableReceiving monitor
-    fd <- getFd monitor
-    forever $ do
-      res <- select' [fd] [] [] Never
-      case res of
-        Just ([_], [], []) -> do
-          dev <- receiveDevice monitor
-          dumpDeviceInfo dev
-        Nothing -> return ()
-    return ()
+main = withUDev $ \ udev -> do
+  setLogPriority udev LogDebug
+  setLogger udev defaultLogger
+  monitor <- newFromNetlink udev UDevId
+  enableReceiving monitor
+  fd <- getFd monitor
+  forever $ do
+    threadWaitRead fd
+    dev <- receiveDevice monitor
+    dumpDeviceInfo dev
